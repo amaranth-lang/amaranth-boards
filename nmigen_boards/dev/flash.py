a3759b11930f234c6cdb7d0dc0b739640e974131
whitequark <whitequark@whitequark.org>
2019-07-05 20:15:49 +0000

    dev.flash: spiflash→spi_flash, spiflash{2x,4x}→spi_flash_{2x,4x}.
    Every other resource name uses underscores for separation, like
    eth_rgmii, so SPI flash should do it too.

--- nmigen_boards/dev/flash.py
+++ nmigen_boards/dev/flash.py
@@ -19,15 +19,15 @@ def SPIFlashResources(number, *, cs, clk, mosi, miso, wp=None, hold=None, attrs=
     if wp is not None and hold is not None:
         io_1x.append(Subsignal("wp",   PinsN(wp,   dir="o")))
         io_1x.append(Subsignal("hold", PinsN(hold, dir="o")))
-    resources.append(Resource("spiflash", number, *io_1x))
+    resources.append(Resource("spi_flash", number, *io_1x))
 
     io_2x = list(io_all)
     io_2x.append(Subsignal("dq", Pins(" ".join([mosi, miso]), dir="io")))
-    resources.append(Resource("spiflash2x", number, *io_2x))
+    resources.append(Resource("spi_flash_2x", number, *io_2x))
 
     if wp is not None and hold is not None:
         io_4x = list(io_all)
         io_4x.append(Subsignal("dq", Pins(" ".join([mosi, miso, wp, hold]), dir="io")))
-        resources.append(Resource("spiflash4x", number, *io_4x))
+        resources.append(Resource("spi_flash_4x", number, *io_4x))
 
     return resources
