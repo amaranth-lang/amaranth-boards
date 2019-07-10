11971f1b13dfbd4e3ba61eb5cc649cee52ef9ac0
William D. Jones <thor0505@comcast.net>
2019-07-10 03:58:43 -0400

    dev.spi: Update SPIResources to accept a name and direction.

--- nmigen_boards/dev/spi.py
+++ nmigen_boards/dev/spi.py
@@ -4,16 +4,30 @@ from nmigen.build import *
 __all__ = ["SPIResource"]
 
 
-def SPIResource(number, *, cs, clk, mosi, miso, int=None, reset=None, attrs=None):
+def SPIResource(*args, cs, clk, mosi, miso, int=None, reset=None, attrs=None, role="host"):
+    assert role in ("host", "device")
+
     io = []
-    io.append(Subsignal("cs", PinsN(cs, dir="o")))
-    io.append(Subsignal("clk", Pins(clk, dir="o")))
-    io.append(Subsignal("mosi", Pins(mosi, dir="o")))
-    io.append(Subsignal("miso", Pins(miso, dir="i")))
+    if role == "host":
+        io.append(Subsignal("cs", PinsN(cs, dir="o")))
+        io.append(Subsignal("clk", Pins(clk, dir="o", assert_width=1)))
+        io.append(Subsignal("mosi", Pins(mosi, dir="o", assert_width=1)))
+        io.append(Subsignal("miso", Pins(miso, dir="i", assert_width=1)))
+    else:  # device
+        io.append(Subsignal("cs", PinsN(cs, dir="i", assert_width=1)))
+        io.append(Subsignal("clk", Pins(clk, dir="i", assert_width=1)))
+        io.append(Subsignal("mosi", Pins(mosi, dir="i", assert_width=1)))
+        io.append(Subsignal("miso", Pins(miso, dir="oe", assert_width=1)))
     if int is not None:
-        io.append(Subsignal("int", Pins(int, dir="i")))
+        if role == "host":
+            io.append(Subsignal("int", Pins(int, dir="i")))
+        else:
+            io.append(Subsignal("int", Pins(int, dir="oe", assert_width=1)))
     if reset is not None:
-        io.append(Subsignal("reset", Pins(reset, dir="o")))
+        if role == "host":
+            io.append(Subsignal("reset", Pins(reset, dir="o")))
+        else:
+            io.append(Subsignal("reset", Pins(reset, dir="i", assert_width=1)))
     if attrs is not None:
         io.append(attrs)
-    return Resource("spi", number, *io)
+    return Resource.family(*args, default_name="spi", ios=io)
