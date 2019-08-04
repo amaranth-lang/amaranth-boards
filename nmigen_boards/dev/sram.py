9b532bef934020cdf25dbcd9eee8bd5b973a8186
William D. Jones <thor0505@comcast.net>
2019-08-04 09:32:34 -0400

    Make oe in SRAMResource optional.

--- nmigen_boards/dev/sram.py
+++ nmigen_boards/dev/sram.py
@@ -4,10 +4,12 @@ from nmigen.build import *
 __all__ = ["SRAMResource"]
 
 
-def SRAMResource(*args, cs, oe, we, a, d, dm=None, attrs=None):
+def SRAMResource(*args, cs, oe=None, we, a, d, dm=None, attrs=None):
     io = []
     io.append(Subsignal("cs", PinsN(cs, dir="o", assert_width=1)))
-    io.append(Subsignal("oe", PinsN(oe, dir="o", assert_width=1)))
+    if oe is not None:
+        # Asserted WE# deactivates the D output buffers, so WE# can be used to replace OE#.
+        io.append(Subsignal("oe", PinsN(oe, dir="o", assert_width=1)))
     io.append(Subsignal("we", PinsN(we, dir="o", assert_width=1)))
     io.append(Subsignal("a", Pins(a, dir="o")))
     io.append(Subsignal("d", Pins(d, dir="io")))
