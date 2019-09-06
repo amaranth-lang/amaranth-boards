f1375702045c2300054f241a3142e0c9540c4b80
whitequark <whitequark@whitequark.org>
2019-09-06 05:24:11 +0000

    setup: replace versioneer with setuptools_scm.

---
+++ nmigen_boards/__init__.py
@@ -0,0 +1,5 @@
+import pkg_resources
+try:
+    __version__ = pkg_resources.get_distribution(__name__).version
+except pkg_resources.DistributionNotFound:
+    pass
--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
-import sys
 from setuptools import setup, find_packages
-import versioneer
+
+
+def scm_version():
+    def local_scheme(version):
+        return version.format_choice("+{node}", "+{node}.dirty")
+    return {"version_scheme": "guess-next-dev", "local_scheme": local_scheme}
 
 
 setup(
     name="nmigen-boards",
-    version=versioneer.get_version(),
+    use_scm_version=scm_version(),
     author="whitequark",
     author_email="whitequark@whitequark.org",
     description="Board and connector definitions for nMigen",
     #long_description="""TODO""",
     license="BSD",
+    setup_requires=["setuptools_scm"],
     install_requires=["nmigen"],
     packages=find_packages(),
     project_urls={
         "Source Code": "https://github.com/m-labs/nmigen-boards",
         "Bug Tracker": "https://github.com/m-labs/nmigen-boards/issues",
     },
-    cmdclass=versioneer.get_cmdclass()
 )
