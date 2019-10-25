ba2911914b076ca7b3ec1394a74e40bf741bc80e
Dan Ravensloft <dan.ravensloft@gmail.com>
2019-10-25 10:20:20 +0100

    Add DE10-Nano-based MiSTer.

--- nmigen_boards/mister.py
+++ nmigen_boards/mister.py
@@ -6,11 +6,11 @@ from nmigen.vendor.intel import *
 from .resources import *
 
 
-__all__ = ["DE10NanoPlatform"]
+__all__ = ["MisterPlatform"]
 
 
 # The MiSTer platform is built around the DE10-Nano; if you update one you should update the other.
-class DE10NanoPlatform(IntelPlatform):
+class MisterPlatform(IntelPlatform):
     device      = "5CSEBA6" # Cyclone V 110K LEs
     package     = "U23"     # UBGA-484
     speed       = "I7"
@@ -61,6 +61,45 @@ class DE10NanoPlatform(IntelPlatform):
             Subsignal("scl", Pins("U10", dir="o")),
             Subsignal("sda", Pins("AA4", dir="io")),
             Attrs(io_standard="3.3-V LVTTL")),
+
+        # MiSTer SDRAM Board (required)
+        # https://github.com/MiSTer-devel/Hardware_MiSTer/blob/master/releases/sdram_xs_2.2.pdf
+        SDRAMResource(0,
+            clk="20", cs="33", we="27", ras="32", cas="31",
+            ba="34 35", a="37 38 39 40 28 25 26 23 24 21 36 22 19",
+            dq="1 2 3 4 5 6 7 8 18 17 16 15 14 13 9 10",
+            dqm="", conn=("gpio", 0), attrs=Attrs(io_standard="3.3-V LVCMOS")),
+
+        # MiSTer I/O Board (optional, but highly recommended)
+        # https://github.com/MiSTer-devel/Hardware_MiSTer/blob/master/releases/iobrd_6.0.pdf
+        Resource("power_led", 0, PinsN("1", dir="o", conn=("gpio", 1)), Attrs(io_standard="3.3-V LVTTL")),
+        Resource("disk_led", 0, PinsN("3", dir="o", conn=("gpio", 1)), Attrs(io_standard="3.3-V LVTTL")),
+        Resource("user_led", 0, PinsN("5", dir="o", conn=("gpio", 1)), Attrs(io_standard="3.3-V LVTTL")),
+
+        Resource("reset_switch", 0, PinsN("17", dir="i", conn=("gpio", 1)), Attrs(io_standard="3.3-V LVTTL")),
+        Resource("osd_switch", 0, PinsN("13", dir="i", conn=("gpio", 1)), Attrs(io_standard="3.3-V LVTTL")),
+        Resource("user_switch", 0, PinsN("15", dir="i", conn=("gpio", 1)), Attrs(io_standard="3.3-V LVTTL")),
+
+        Resource("audio", 0,
+            Subsignal("l", Pins("2", dir="o", conn=("gpio", 1))),
+            Subsignal("r", Pins("7", dir="o", conn=("gpio", 1))),
+            Attrs(io_standard="3.3-V LVTTL")),
+
+        Resource("toslink", 0, Pins("9", dir="o", conn=("gpio", 1))),
+
+        *SDCardResources(0,
+            clk="13", cmd="6", 
+            dat0="16", dat1="18", dat2="4", dat3="6",
+            conn=("gpio", 1), attrs=Attrs(io_standard="3.3-V LVTTL")),
+
+        # The schematic is difficult to understand here...
+        Resource("vga", 0,
+            Subsignal("r", Pins("28 32 34 36 38 40", dir="o", conn=("gpio", 1))),
+            Subsignal("g", Pins("27 31 33 35 37 39", dir="o", conn=("gpio", 1))),
+            Subsignal("b", Pins("21 23 25 26 24 24", dir="o", conn=("gpio", 1))),
+            Subsignal("hs", Pins("20", dir="o", conn=("gpio", 1))),
+            Subsignal("vs", Pins("19", dir="o", conn=("gpio", 1))),
+            Attrs(io_standard="3.3-V LVTTL"))
     ]
     connectors  = [
         # Located on the top of the board, above the chip.
@@ -92,4 +131,4 @@ class DE10NanoPlatform(IntelPlatform):
 
 if __name__ == "__main__":
     from .test.blinky import Blinky
-    DE10NanoPlatform().build(Blinky(), do_program=True)
+    MisterPlatform().build(Blinky(), do_program=True)
