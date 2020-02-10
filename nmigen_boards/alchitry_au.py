7c7d47e6f28487ddb1e320fff88dd1269519f6c5
whitequark <whitequark@whitequark.org>
2020-02-10 01:22:46 +0000

    style/formatting fixes

--- nmigen_boards/alchitry_au.py
+++ nmigen_boards/alchitry_au.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import shutil
 
 from nmigen.build import *
 from nmigen.vendor.xilinx_7series import *
@@ -8,8 +9,8 @@ from .resources import *
 
 __all__ = ["AlchitryAuPlatform"]
 
+
 def find_loader():
-    import shutil # Delay this until we're actually trying to program
     loader_prgm = os.environ.get("ALCHITRY_LOADER", shutil.which("loader"))
     if loader_prgm is None:
         raise EnvironmentError("Could not find Alchrity Loader. Place "
@@ -61,15 +62,14 @@ class AlchitryAuPlatform(Xilinx7SeriesPlatform):
     ]
 
     connectors  = [
-        Connector("bank", 0, "T8  T7  T5  R5  R8  P8  L2  L3  J1  K1  H1  H2  G1  G2  K5  E6"
-                             "T10 T9  R6  R7  P9  N9  K2  K3  J4  J5  H3  J3  H4  H5  N6  M6"),
-        Connector("bank", 1, "D1  E2  A2  B2  E1  F2  F3  F4  A3  B4  A4  A5  B5  B6  A7  B7"
-                             "B1  C1  C2  C3  D3  E3  C4  D4  G4  G5  E5  F5  D5  D6  C6  C7"),
-        Connector("bank", 2, "T13 R13 T12 R12 R11 R10 N2  N3  P3  P4  M4  L4  N4  M5  L5  P5"
-                             "P11 P10 N12 N11 P13 N13 M1  M2  P1  N1  R1  R2  T2  R3  T3  T4"),
-        # There's a bunch of non-io lines in bank 4. Might need a better way to represent this
-        Connector("bank", 3, "L14 L13 M12 N16 R16 R15 P14 M15 P16 P15  -   -   -   -   -   -"
-                             "K13 K12 M16 M14 T16 T14 N14   -   -   -  -   -   -   -   -   -")
+        Connector("bank", 0, "T8  T7  T5  R5  R8  P8  L2  L3  J1  K1  H1  H2  G1  G2  K5  E6 "
+                             "T10 T9  R6  R7  P9  N9  K2  K3  J4  J5  H3  J3  H4  H5  N6  M6 "),
+        Connector("bank", 1, "D1  E2  A2  B2  E1  F2  F3  F4  A3  B4  A4  A5  B5  B6  A7  B7 "
+                             "B1  C1  C2  C3  D3  E3  C4  D4  G4  G5  E5  F5  D5  D6  C6  C7 "),
+        Connector("bank", 2, "T13 R13 T12 R12 R11 R10 N2  N3  P3  P4  M4  L4  N4  M5  L5  P5 "
+                             "P11 P10 N12 N11 P13 N13 M1  M2  P1  N1  R1  R2  T2  R3  T3  T4 "),
+        Connector("bank", 3, "L14 L13 M12 N16 R16 R15 P14 M15 P16 P15  -   -   -   -   -   - "
+                             "K13 K12 M16 M14 T16 T14 N14   -   -   -  -   -   -   -   -   - ")
     ]
 
     def toolchain_program(self, products, name):
@@ -82,5 +82,4 @@ class AlchitryAuPlatform(Xilinx7SeriesPlatform):
 
 if __name__ == "__main__":
     from .test.blinky import Blinky
-    p = AlchitryAuPlatform()
-    p.build(Blinky(), do_program=True)
+    AlchitryAuPlatform().build(Blinky(), do_program=True)
