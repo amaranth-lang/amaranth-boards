85a5d80bf29337e5d28edc6f6ccaf73f70b539f6
Stuart Olsen <stuart@sj-olsen.com>
2020-03-14 02:02:03 -0700

    resources.display: Apply inversion setting to dp

--- nmigen_boards/resources/display.py
+++ nmigen_boards/resources/display.py
@@ -15,7 +15,7 @@ def Display7SegResource(*args, a, b, c, d, e, f, g, dp=None, invert=False,
     ios.append(Subsignal("f", Pins(f, dir="o", invert=invert, conn=conn, assert_width=1)))
     ios.append(Subsignal("g", Pins(g, dir="o", invert=invert, conn=conn, assert_width=1)))
     if dp is not None:
-        ios.append(Subsignal("dp", Pins(dp, dir="o", conn=conn, assert_width=1)))
+        ios.append(Subsignal("dp", Pins(dp, dir="o", invert=invert, conn=conn, assert_width=1)))
     if attrs is not None:
         ios.append(attrs)
     return Resource.family(*args, default_name="display_7seg", ios=ios)
