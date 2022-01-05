aaf18252e457ff95257137da2a629820c0ff2bfa
Catherine <whitequark@whitequark.org>
2022-01-05 06:06:38 +0000

    setup: relax dependency on amaranth.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     license="BSD",
     setup_requires=["wheel", "setuptools", "setuptools_scm"],
     install_requires=[
-        "amaranth>=0.2,<0.4",
+        "amaranth>=0.2,<0.5",
         "importlib_metadata; python_version<'3.8'",
     ],
     packages=find_packages(),
